38fc518cbc024c2c3121063cbd282d71ff632774
syamsec <121809593+syamsec@users.noreply.github.com>
2023-03-19 23:47:05 +0700

    Create lingkaran.py

---
+++ Bangun Datar/lingkaran.py
@@ -0,0 +1,12 @@
+# input jari-jari lingkaran
+jari_jari = float(input("Masukkan jari-jari lingkaran: "))
+
+# hitung luas lingkaran
+luas = 3.14 * jari_jari * jari_jari
+
+# hitung keliling lingkaran
+keliling = 2 * 3.14 * jari_jari
+
+# tampilkan hasil
+print("Luas lingkaran adalah: ", luas)
+print("Keliling lingkaran adalah: ", keliling)
