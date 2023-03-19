31350472d173bbbe56c7ca740d6e101fc85f9d4e
syamsec <121809593+syamsec@users.noreply.github.com>
2023-03-20 00:02:40 +0700

    Create kubus.py

---
+++ Bangun Ruang/kubus.py
@@ -0,0 +1,16 @@
+# input panjang sisi kubus
+sisi = float(input("Masukkan panjang sisi kubus: "))
+
+# hitung luas permukaan kubus
+luas_permukaan = 6 * sisi ** 2
+
+# hitung keliling kubus
+keliling = 12 * sisi
+
+# hitung volume kubus
+volume = sisi ** 3
+
+# tampilkan hasil
+print("Luas permukaan kubus = ", luas_permukaan)
+print("Keliling kubus = ", keliling)
+print("Volume kubus = ", volume)
