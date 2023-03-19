4be1238d2c7adbc39a37f95829874492c1117dbd
syamsec <121809593+syamsec@users.noreply.github.com>
2023-03-19 23:53:06 +0700

    Create bola.py

---
+++ Bangun Ruang/bola.py
@@ -0,0 +1,9 @@
+jari_jari = float(input("Masukkan jari-jari bola: "))
+
+pi = 3.14
+
+luas_permukaan = 4 * pi * jari_jari ** 2
+volume = 4/3 * pi * jari_jari ** 3
+
+print("Luas permukaan bola adalah:", luas_permukaan)
+print("Volume bola adalah:", volume)
