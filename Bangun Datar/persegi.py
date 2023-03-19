bb02f330a0517bf2c90c2177050c71d506bccaf9
syamsec <121809593+syamsec@users.noreply.github.com>
2023-03-19 23:48:50 +0700

    Create persegi.py

---
+++ Bangun Datar/persegi.py
@@ -0,0 +1,8 @@
+panjang = float(input("Masukkan panjang persegi panjang: "))
+lebar = float(input("Masukkan lebar persegi panjang: "))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+print("Luas persegi panjang adalah:", luas)
+print("Keliling persegi panjang adalah:", keliling)
